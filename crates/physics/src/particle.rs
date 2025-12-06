dc523f59c0b072d50e0ca80fe8bd95e798a94106
Fabian <fabi.dierksheide@web.de>
2025-12-06 22:58:11 +0100

    Week 1 basic physics implementation

---
+++ crates/physics/src/particle.rs
@@ -0,0 +1,16 @@
+use glam::Vec2;
+
+
+pub struct Particle {
+    pub pos: Vec2,
+    pub vel: Vec2, 
+}
+
+impl Particle{
+    pub fn new(pos: Vec2) -> Self{
+        Self{
+            pos,
+            vel: Vec2::ZERO,  //Zero for 1. weeks tests-> velocity in week2 
+        }
+    }
+}
---
+++ crates/physics/src/world.rs
@@ -0,0 +1,45 @@
+//use  glam::Vec2;
+use  crate::Particle;
+
+pub struct World{
+    pub particles: Vec<Particle>,
+}
+
+impl World{
+
+    pub fn new()->Self  {
+        Self{
+            particles: Vec::new(),
+        }
+    } 
+
+    pub fn add_particle(&mut self, p: Particle) -> usize {
+        self.particles.push(p);
+        self.particles.len() - 1            //stellen rückgabe des eingefügten particels
+    }
+
+    pub fn update_positions(&mut self) {
+        for p in &mut self.particles {
+            p.pos += p.vel;    
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    use glam::Vec2;
+    use crate::Particle;
+
+    #[test]
+    fn test_1_particle_moves(){
+        let mut world = World::new();
+        let id = world.add_particle(Particle::new(Vec2::new(0.0,0.0)));
+        world .particles[id].vel = Vec2::new(1.0,1.0);
+
+        world.update_positions(); 
+        assert_eq!(world.particles[id].pos, Vec2::new(1.0,1.0));
+    }
+}
+
