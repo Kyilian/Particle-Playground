13fadc2b36662aff9d4f63175517eaff723c92e2
Timo L <timoliohd@gmail.com>
2025-12-08 00:15:42 +0100

    Week 1 basic window implementation | created window.rs and changed some dependency versions

--- crates/render/src/lib.rs
+++ crates/render/src/lib.rs
@@ -1,6 +1,14 @@
 pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+    left + right // what is this for?
 }
+
+mod window;
+
+pub use window::RenderWindow;
+pub use wgpu;
+
+pub const WINDOW_WIDTH: u32 = 800;
+pub const WINDOW_HEIGHT: u32 = 600;
 
 #[cfg(test)]
 mod tests {
