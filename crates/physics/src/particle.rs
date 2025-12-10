18099d79da6a0d0df5ffd714d421b19ab1a85f5d
Fabian <fabi.dierksheide@web.de>
2025-12-10 22:38:03 +0100

    Week 2 : Verlet integration + Circle Collider basic

--- crates/physics/src/particle.rs
+++ crates/physics/src/particle.rs
@@ -3,14 +3,21 @@ use glam::Vec2;
 #[derive(Debug, Clone, Copy)]
 pub struct Particle {
     pub pos: Vec2,
-    pub vel: Vec2,
+    pub old_pos: Vec2,
+    pub acc: Vec2, // force z.b gravity
 }
 
 impl Particle {
+    /// Creates a new particle at the given position.
     pub fn new(pos: Vec2) -> Self {
         Self {
             pos,
-            vel: Vec2::ZERO, //Zero for 1. weeks tests-> velocity in week2
+            old_pos: pos,
+            acc: Vec2::ZERO,
         }
+    }
+
+    pub fn add_force(&mut self, force: Vec2) {
+        self.acc += force;
     }
 }
